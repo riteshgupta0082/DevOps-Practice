4297b6b317ca0d82ca823baf3272fa7ab70b45c9
Ritesh Gupta <riteshgupta0082@gmail.com>
2026-08-23 11:59:09 +0530

    rg2 added

--- main.tf
+++ main.tf
@@ -1,20 +1,24 @@
 terraform {
   required_providers {
-    azurerm ={
-        source = "hashicorp/azurerm"
-        version = "5.0.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "rgs" {
-  name = "rgname"
+  name     = "rgname"
+  location = "eastus"
+}
+resource "azurerm_resource_group" "rgs2" {
+  name     = "rgname2"
   location = "eastus"
 }
 

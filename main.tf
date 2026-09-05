b3e02169375363a166a82a2d4484f3c6f1ae1695
Ritesh Gupta <riteshgupta0082@gmail.com>
2026-09-05 19:02:47 +0530

    one rg added

--- main.tf
+++ main.tf
@@ -21,6 +21,10 @@ resource "azurerm_resource_group" "rgs2" {
   name     = "rgname2"
   location = "eastus"
 }
+resource "azurerm_resource_group" "rgs3" {
+  name     = "rgname3"
+  location = "eastus"
+}
 
 
 
